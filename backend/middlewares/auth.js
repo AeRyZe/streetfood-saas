7d31e9ba7d1f153e9dcb9910906d5069934bd2f1
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2024-08-22 23:29:44 +0200

    fix(auth): authentication fixed

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,7 +7,7 @@ export default (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.VITE_JWT_TOKEN);
-        const userId = decodedToken._id;
+        const userId = decodedToken.userId;
         const isCompany = !!decodedToken.siret; // '!!' convertit en boolean
 
         req.auth = {
